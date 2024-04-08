bae52ec11b5541669e96c052b3cf320ad961bdc2
Grayson <GMcClead@sportsbusinessjournal.com>
2024-04-08 09:59:49 -0500

    generation working

--- TileMap.gd
+++ TileMap.gd
@@ -14,8 +14,8 @@ func generateMap():
 	noise.fractal_lacunarity = 2.0
 	noise.fractal_gain = 0.5
 	
-	for x in range(get_used_rect().size.x):
-		for y in range(get_used_rect().size.y):
+	for x in range(100):
+		for y in range(100):
 			var coords: Vector2 = Vector2(x, y)
 			var noise_value = noise.get_noise_2dv(coords)
 			var tile_index = determine_tile_index(noise_value)
@@ -24,6 +24,8 @@ func generateMap():
 func determine_tile_index(value: float) -> Vector2:
 	if value < 0.2:
 		return Vector2(0,0)
+	elif value > 0.2 && value < 0.5:
+		return Vector2(3,0)
 	else:
 		return Vector2(2,1)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
