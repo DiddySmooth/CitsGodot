9cd16fe9cf6e5076f5abd2bdff32609003855eb8
Grayson McClead <gmcclead@sportsbusinessjournal.com>
2024-04-08 19:50:48 -0500

    generator

--- TileMap.gd
+++ TileMap.gd
@@ -9,25 +9,39 @@ func _ready():
 func generateMap():
 	noise = FastNoiseLite.new()
 	noise.seed = 123
-	noise.frequency = 0.02
+	noise.frequency = 0.009
 	noise.fractal_octaves = 4
 	noise.fractal_lacunarity = 2.0
 	noise.fractal_gain = 0.5
 	
-	for x in range(100):
-		for y in range(100):
+	for x in range(1000):
+		for y in range(1000):
 			var coords: Vector2 = Vector2(x, y)
 			var noise_value = noise.get_noise_2dv(coords)
 			var tile_index = determine_tile_index(noise_value)
 			set_cell(0, coords, 0, tile_index)
 	pass
 func determine_tile_index(value: float) -> Vector2:
-	if value < 0.2:
+	if value < -.8:
 		return Vector2(0,0)
-	elif value > 0.2 && value < 0.5:
+	elif value >= -.8 && value < -0.6:
+		return Vector2(1,0)
+	elif value >= -.6 && value < -.4:
+		return Vector2(2,0)
+	elif value >= -.4 && value < -.2:
 		return Vector2(3,0)
-	else:
+	elif value >= -.2 && value < 0:
+		return Vector2(0,1)
+	elif value >= 0 && value < 0.2:
+		return Vector2(1,1)
+	elif value >= 0.2 && value < 0.4:
 		return Vector2(2,1)
+	elif value >= 0.4 && value < 0.6:
+		return Vector2(0,2)
+	elif value >= 0.6 && value < 0.8:
+		return Vector2(2,2)
+	else:
+		return Vector2(3,2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
