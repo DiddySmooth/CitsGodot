5aa9a237ba7b81b3768beae34d280722fa995d2f
Grayson McClead <gmcclead@sportsbusinessjournal.com>
2024-04-12 18:51:52 -0500

    added cit class need to work on more

--- Scripts/CitManager.gd
+++ Scripts/CitManager.gd
@@ -1,12 +1,20 @@
 extends Node
 
+var custom_instance = preload("res://Scripts/Classes/Cit.gd").new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	custom_instance.citCha = 5
+	custom_instance.getStats()
 	pass # Replace with function body.
 
-func createCits():
+func createStarterCits():
 	pass
+
+func createCit():
+	pass
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
---
+++ Scripts/Classes/Cit.gd
@@ -0,0 +1,22 @@
+class_name Cit
+
+extends Node
+
+var citName
+var citRace
+var citStr
+var citDex 
+var citCon
+var citInt
+var citWis
+var citCha
+
+
+
+func getStats():
+	print(citStr)
+	print(citDex)
+	print(citCon)
+	print(citInt)
+	print(citWis)
+	print(citCha)
