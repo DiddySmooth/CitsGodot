b22742e424e975cd42c323fd77cbe01711ef8ac3
Grayson <GMcClead@sportsbusinessjournal.com>
2024-04-13 16:43:12 -0500

    added names text file need to figure out how to read it

--- Scripts/Classes/Cit.gd
+++ Scripts/Classes/Cit.gd
@@ -2,11 +2,10 @@ class_name Cit
 
 extends Node
 
-
-
-
 var citName
 var citRace
+var citJob
+
 var citStr
 var citDex 
 var citCon
@@ -26,6 +25,10 @@ func generateStats():
 	citWis = randi_range(8, 20)
 	citInt = randi_range(8, 20)
 	citCha = randi_range(8, 20)
+
+func generateName():
+	citName = "Grayson"
+
 
 func getStats():
 	print(citStr)
