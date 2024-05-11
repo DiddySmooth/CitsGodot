3388b210e1a2412beac027dec17a51bce3eab1b7
Grayson <GMcClead@sportsbusinessjournal.com>
2024-05-11 14:57:18 -0500

    stuff

--- Scripts/UIManager.gd
+++ Scripts/UIManager.gd
@@ -1,11 +1,26 @@
 extends Control
 
+var npc_list = [] # Your list of NPCs (NPC objects or dictionaries)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	var column1 = Label.new()
+	column1.text = "Name"
+	var column2 = Label.new()
+	column2.text = "Race"
+	var column3 = Label.new()
+	column3.text = "Age"
+	$GridContainer.add_child(column1)
+	$GridContainer.add_child(column2)
+	$GridContainer.add_child(column3)
 	pass
+
+
+
+func updateList():
+	var column1 = Label.new()
+	column1.text = "Name"
+	var column2 = Label.new()
+	column2.text = "Race"
+	var column3 = Label.new()
+	column3.text = "Age"
+# Function to update the grid with NPC data
