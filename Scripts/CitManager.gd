1e3dda5c5918336190c3f4df7f08e4ba780ed3e2
Grayson <GMcClead@sportsbusinessjournal.com>
2024-05-13 11:21:31 -0500

    list ui

--- Scripts/CitManager.gd
+++ Scripts/CitManager.gd
@@ -3,15 +3,18 @@ extends Node
 
 var starterNumber = 6
 var cits = []
+var controlNode
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	controlNode = get_parent().get_node("Control")
+	print(controlNode)
 	createStarterCits()
-	print(cits)
 	pass # Replace with function body.
 
 func createStarterCits():
 	for i in range(starterNumber):
 		cits.append(createCit())
+	controlNode.updateList(cits)
 
 func createCit():
 	var cit = preload("res://Scripts/Classes/Cit.gd").new()
--- Scripts/UIManager.gd
+++ Scripts/UIManager.gd
@@ -3,24 +3,34 @@ extends Control
 var npc_list = [] # Your list of NPCs (NPC objects or dictionaries)
 
 func _ready():
-	var column1 = Label.new()
-	column1.text = "Name"
-	var column2 = Label.new()
-	column2.text = "Race"
-	var column3 = Label.new()
-	column3.text = "Age"
-	$GridContainer.add_child(column1)
-	$GridContainer.add_child(column2)
-	$GridContainer.add_child(column3)
 	pass
 
 
 
-func updateList():
+func updateList(citList):
+	for n in $GridContainer.get_children():
+		$GridContainer.remove_child(n)
+		n.queue_free() 
+		
 	var column1 = Label.new()
 	column1.text = "Name"
 	var column2 = Label.new()
 	column2.text = "Race"
 	var column3 = Label.new()
 	column3.text = "Age"
+	
+	$GridContainer.add_child(column1)
+	$GridContainer.add_child(column2)
+	$GridContainer.add_child(column3)
+	
+	for i in citList:
+		var name = Label.new()
+		name.text = i.citName
+		var race = Label.new()
+		race.text = i.citRace
+		var age = Label.new()
+		age.text = str(i.citAge)
+		$GridContainer.add_child(name)
+		$GridContainer.add_child(race)
+		$GridContainer.add_child(age)
 # Function to update the grid with NPC data
